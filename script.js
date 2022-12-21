ee100fa44bfb71e5af9fdefbd0ce0b0cc53684dd
Aspen Wilson <aspen.schell@gmail.com>
2022-12-21 10:49:59 -0800

    add script.js, connect to index.html

---
+++ script.js
@@ -0,0 +1,8 @@
+const h2 = document.createElement("h2");
+h2.textContent = "This content added by JavaScript";
+document.querySelector("body").appendChild(h2);
+
+const h3 = document.createElement("h3");
+h3.textContent = "Javascript is harder than HTML";
+document.querySelector("body").appendChild(h3);
+
